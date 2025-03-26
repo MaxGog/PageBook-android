af63b0bce30753d28174bf8425a7807efeaefadc
MaxGog <max.gog2005@outlook.com>
2025-03-26 18:23:25 +0300

    Исправление NoteListScreen
    Изменена структура интерфейса, чтобы плавающая кнопка не расширялась на весь экран.

--- PageBook.Android/app/src/main/java/ru/maxgog/pagebook/ui/NoteListScreen.kt
+++ PageBook.Android/app/src/main/java/ru/maxgog/pagebook/ui/NoteListScreen.kt
@@ -1,12 +1,16 @@
 package ru.maxgog.pagebook.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import ru.maxgog.pagebook.models.NoteModel
 
 @Composable
@@ -15,14 +19,22 @@ fun NoteListScreen(
     onNoteClick: (Int) -> Unit,
     onAddNote: () -> Unit
 ) {
-    LazyColumn {
-        items(notes.size) { index ->
-            val note = notes[index]
-            NoteItem(note = note, onClick = { onNoteClick(note.id) })
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = onAddNote) {
+                Icon(Icons.Default.Add, contentDescription = "Add Note")
+            }
         }
-    }
-
-    FloatingActionButton(onClick = onAddNote) {
-        Icon(Icons.Default.Add, contentDescription = "Add Note")
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            items(notes.size) { index ->
+                val note = notes[index]
+                NoteItem(note = note, onClick = { onNoteClick(note.id) })
+            }
+        }
     }
 }
