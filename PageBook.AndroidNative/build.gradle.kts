5342d6cfb19d824f7062eeaceef31e9256172ab8
MaxGog <max.gog2005@outlook.com>
2025-03-28 23:30:04 +0300

    Обновление нативного проекта андроида

--- PageBook.AndroidNative/build.gradle.kts
+++ PageBook.AndroidNative/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 buildscript {
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("com.android.tools.build:gradle:8.9.1")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
