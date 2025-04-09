ec709ff3be4fb0bd524af72cd7ad25ab0ac87cb9
MaxGog <max.gog2005@outlook.com>
2025-04-09 10:34:49 +0300

    Добавление конвектора

---
+++ PageBook.AndroidNative/app/src/main/java/ru/maxgog/pagebook/Convectors.kt
@@ -0,0 +1,16 @@
+package ru.maxgog.pagebook
+
+import androidx.room.TypeConverter
+import kotlinx.datetime.LocalDate
+
+class Converters {
+    @TypeConverter
+    fun fromTimestamp(value: String?): LocalDate? {
+        return value?.let { LocalDate.parse(it) }
+    }
+
+    @TypeConverter
+    fun dateToTimestamp(date: LocalDate?): String? {
+        return date?.toString()
+    }
+}
